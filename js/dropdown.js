e9c60fa2016342582957756583f0d0eb8a94a344
mikmak29 <mjogamaapple@gmail.com>
2025-07-03 22:32:02 +0800

    feat responsive navbar

---
+++ js/dropdown.js
@@ -0,0 +1,20 @@
+const navBar = () => {
+    const nav_main = document.getElementById("nav_main");
+
+    if (nav_main) {
+        nav_main.classList.toggle("nav-show");
+    } else {
+        console.error("Can't find the Element");
+    }
+}
+
+const navList = () => {
+    const nav_list = document.getElementById("nav_list");
+
+    if (nav_list) {
+        nav_list.classList.toggle("nav-list-tablet-mode");
+        nav_list.classList.toggle("nav-list-show");
+    } else {
+        console.error("Can't find the Element");
+    }
+}
