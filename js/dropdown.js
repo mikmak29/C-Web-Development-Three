28d2f1ccc500ede50eb7b8cf411c4d71fca290b9
mikmak29 <mjogamaapple@gmail.com>
2025-07-13 22:12:10 +0800

    navbar fixed

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,16 +1,7 @@
-const navBar = () => {
-    const nav_main = document.getElementById("nav_main");
-
-    if (nav_main) {
-        nav_main.classList.toggle("nav-show")
-    }
-}
-
-const navList = () => {
-    const nav_list = document.getElementById("nav_list");
+const navbarListButton = () => {
+    const nav_list = document.getElementById("navbar_list");
 
     if (nav_list) {
-        nav_list.classList.toggle("nav-list-tablet-mode");
         nav_list.classList.toggle("nav-list-show");
     } else {
         console.error("Can't find the Element");
